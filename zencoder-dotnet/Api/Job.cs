2cb035aadcf50eda9426088765722e9af7cabbd1
Adam Kittelson <adam@zencoder.com>
2010-09-15 14:44:01 -0500

    initial commit

---
+++ zencoder-dotnet/Api/Job.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace ZencoderDotNet.Api
+{
+    public class Job
+    {
+        public string create(string body)
+        {
+            string jobUrl = Zencoder.BaseUrl + "/jobs";
+            
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(jobUrl);
+            req.Proxy = null;
+            
+            if (Zencoder.format == "xml")
+            {
+                req.ContentType = "application/xml";
+                req.Accept = "application/xml";
+            }
+            else
+            {
+                req.Accept = "application/json";
+                req.ContentType = "application/json";
+            }
+            byte[] reqData = Encoding.UTF8.GetBytes(body);
+            req.ContentLength = reqData.Length;
+            req.Method = "POST";
+
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(reqData, 0, reqData.Length);
+            }
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+
+            string responseBody = String.Empty;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                using (StreamReader readStream = new StreamReader(responseStream, encode))
+                {
+                    Char[] read = new Char[256];
+                    int count = readStream.Read(read, 0, 256);
+                    
+                    while (count > 0)
+                    {
+                        string str = new String(read, 0, count);
+                        responseBody += str;
+                        count = readStream.Read(read, 0, 256);
+                    }
+                }
+            }
+
+            response.Close();
+            return responseBody;
+        }
+    }
+}
---
+++ zencoder-dotnet/Zencoder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZencoderDotNet
+{
+    public class Zencoder
+    {
+        public static string BaseUrl = "https://zencoder-qa.heroku.com/api";
+        public static string format = "json";
+        private static string apiKey;
+
+        public static string ApiKey
+        {
+            get
+            {
+                if (apiKey == null)
+                {
+                    return Environment.GetEnvironmentVariable("ZENCODER_API_KEY");
+                }
+                else
+                {
+                    return apiKey;
+                }
+            }
+            set { apiKey = value; }
+        }
+    }
+}
